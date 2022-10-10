03be595f6d429bde2de0a50b3f8cd33411350bb2
Brian Albert <brian.david.albert@gmail.com>
2022-10-10 10:30:49 -0400

    added card images, card styling, card shuffle function

--- memory-dictator/src/App.js
+++ memory-dictator/src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react'
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/idi-amin.jpg" },
+  { "src": "/img/kim_jong_il_portrait-2.jpg" },
+  { "src": "/img/peron.jpg" },
+  { "src": "/img/qaddafi.jpg"  },
+  { "src": "/img/stalin.jpg" },
+  { "src": "/img/trujillo.jpg"  }
+]
+
+
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Memory Dictator</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src='/img/5 United Nations emblem.jpg' alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
